b3ae825a6323f46e43f0406686aa693b6e182f5b
Alexa <alexas0202@gmail.com>
2020-10-26 14:52:03 -0500

    Added Primary Key
    Added primary key of shirtID

---
+++ shirts.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Shirts
+(shirtID int AUTO_INCREMENT UNIQUE,
+shirtColor varchar (20) NOT NULL,
+shirtFit varchar (20) NOT NULL,
+shirtDateOfWear date NOT NULL,
+PRIMARY KEY shirtID) ENGINE=INNODB;
+
+
+
