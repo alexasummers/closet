871d1aafc871128c340723f62064ddbe08681cee
Alexa <alexas0202@gmail.com>
2020-10-26 14:57:51 -0500

    Finalized fields
    Finalized fields and created first entry

--- shirts.sql
+++ shirts.sql
@@ -1,9 +1,15 @@
-CREATE TABLE Shirts
-(shirtID int AUTO_INCREMENT UNIQUE,
-shirtColor varchar (20) NOT NULL,
-shirtFit varchar (20) NOT NULL,
-shirtDateOfWear date NOT NULL,
-PRIMARY KEY shirtID) ENGINE=INNODB;
+-- DROP TABLE IF EXISTS shirts;
 
+-- CREATE TABLE Shirts
+-- (shirtID int AUTO_INCREMENT UNIQUE,
+-- shirtDescription varchar (255) NOT NULL,
+-- shirtColor varchar (20) NOT NULL,
+-- shirtFit varchar (20) NOT NULL,
+-- shirtDateOfWear date NOT NULL,
+-- PRIMARY KEY (shirtID)) ENGINE=INNODB;
 
+-- INSERT INTO Shirts (shirtDescription, shirtColor, shirtFit, shirtDateOfWear) VALUES
+-- ('JayHacks', 'Gray', 'Mid', '2020/10/26');
 
+SELECT *
+FROM Shirts
