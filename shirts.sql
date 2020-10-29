50ec5450d5f18b8ace9e0aa6349f047359b78b51
Alexa <alexas0202@gmail.com>
2020-10-28 23:25:23 -0500

    Fixed FK Bug

--- shirts.sql
+++ shirts.sql
@@ -1,19 +1,20 @@
 -- DROP TABLE IF EXISTS shirts;
 
--- CREATE TABLE Shirts
--- (shirtID int AUTO_INCREMENT UNIQUE,
--- shirtDescription varchar (255) NOT NULL,
--- shirtColor varchar (20) NOT NULL,
--- shirtFit varchar (20) NOT NULL,
--- shirtDateOfWear date NOT NULL,
--- PRIMARY KEY (shirtID)) ENGINE=INNODB;
+CREATE TABLE Shirts
+(shirtID int AUTO_INCREMENT UNIQUE,
+shirtDescription varchar (255) NOT NULL,
+shirtColor INT,
+shirtFit varchar (20) NOT NULL,
+shirtDateOfWear date NOT NULL,
+PRIMARY KEY (shirtID),
+FOREIGN KEY (shirtColor) REFERENCES Colors(colorID)) ENGINE=INNODB;
 
 -- INSERT INTO Shirts (shirtDescription, shirtColor, shirtFit, shirtDateOfWear) VALUES
 -- ('Tanktop', 7, 'Fitted', '2020/10/27'),
 -- ('Knit Sweater', 13, 'Large', '2020/10/28');
 
-ALTER TABLE Shirts
-MODIFY COLUMN shirtColor INT(11); 
+-- ALTER TABLE Shirts
+-- MODIFY COLUMN shirtColor INT(11); 
 
 -- ALTER TABLE shirts
 -- ADD FOREIGN KEY (shirtColor) REFERENCES Colors(colorID);
