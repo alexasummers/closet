10c8e1cde474ae1dff496518ad4fb7a6429eb45c
Alexa <alexas0202@gmail.com>
2020-10-31 23:56:05 -0500

    Reinstated shirts table

--- shirts.sql
+++ shirts.sql
@@ -1,17 +1,17 @@
 -- DROP TABLE IF EXISTS shirts;
 
--- CREATE TABLE Shirts
--- (shirtID int AUTO_INCREMENT UNIQUE,
--- shirtDescription varchar (255) NOT NULL,
--- shirtColor INT,
--- shirtFit varchar (20) NOT NULL,
--- shirtDateOfWear date NOT NULL,
--- PRIMARY KEY (shirtID),
--- FOREIGN KEY (shirtColor) REFERENCES Colors(colorID)) ENGINE=INNODB;
+CREATE TABLE Shirts
+(shirtID int AUTO_INCREMENT UNIQUE,
+shirtDescription varchar (255) NOT NULL,
+shirtColor INT,
+shirtFit varchar (20) NOT NULL,
+shirtDateOfWear date NOT NULL,
+PRIMARY KEY (shirtID)) ENGINE=INNODB;
+-- FOREIGN KEY (shirtColor) REFERENCES Colors(colorID)
 
 INSERT INTO Shirts (shirtDescription, shirtColor, shirtFit, shirtDateOfWear) VALUES
--- ('Tanktop', 7, 'Fitted', '2020/10/27'),
--- ('Knit Sweater', 13, 'Large', '2020/10/28');
+('Tanktop', 7, 'Fitted', '2020/10/27'),
+('Knit Sweater', 13, 'Large', '2020/10/28');
 ('Tanktop', 7, 'Fitted', '2020/10/31');
 
 -- ALTER TABLE Shirts
